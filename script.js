974a52eb2e439320a433d79c7830d5cc18baf50f
vcanoramos83 <vcanoramos83@gmail.com>
2025-03-08 20:08:14 +0100

    Se ha mejorado la funcionalidad del formulario

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ if (horas >= 6 && horas < 12) {
 
 // Mostrar el mensaje en el h1
 document.getElementById("bienvenida").textContent = mensajeBienvenida;
+
+
+// Validar el formulario de contacto
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    const nombre = document.getElementById("nombre").value;
+    const email = document.getElementById("email").value;
+    const mensaje = document.getElementById("mensaje").value;
+
+    if (!nombre || !email || !mensaje) {
+        alert("Todos los campos son obligatorios.");
+        event.preventDefault();  // Evita que el formulario se envíe
+    }
+});
